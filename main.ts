a72ba08abc3781cfcded1df57b38088a72809fa6
ilovekoreans <165197639+ilovekoreans@users.noreply.github.com>
2024-08-14 14:25:17 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,38 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showNumber(pins.analogReadPin(AnalogPin.P1))
+})
+let leitura_solo = 0
+basic.showIcon(IconNames.Heart)
+basic.pause(500)
+basic.forever(function () {
+    leitura_solo = pins.digitalReadPin(DigitalPin.P1)
+    if (leitura_solo < 1000) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(2000)
+    } else if (leitura_solo < 650) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(2000)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+        basic.pause(2000)
+    }
+})
